d11f06dae94491672f8ba438968f0fc80cfe7197
ShivaniF2000 <119884719+ShivaniF2000@users.noreply.github.com>
2023-04-21 11:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// your code here
+      const form = document.querySelector('form');
+      const button = document.querySelector('#button');
+      const url = document.querySelector('#url');
+
+
+      button.addEventListener('click', function(event) {
+        event.preventDefault(); // prevent form submission
+
+        // get input values
+        const name = document.querySelector('#name').value;
+        const year = document.querySelector('#year').value;
+
+        // construct query string
+        const query = '?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
+
+        // update URL in h3 element
+        url.textContent = 'https://localhost:8080/' + query;
+      });
